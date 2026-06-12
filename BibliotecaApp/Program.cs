da20a6699023fc8e5eaa98b15504bc17313d0c72
keniaaide <vallesteroskeniaaide@gmail.com>
2026-06-12 16:40:37 -0600

    222

--- BibliotecaApp/Program.cs
+++ BibliotecaApp/Program.cs
@@ -2,14 +2,10 @@ namespace BibliotecaApp
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            // Para soportar acentos y caracteres especiales en español
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
